efeb12da903b028dea20a5d97bf6ee98117aa432
jdelc <javierdelcarril@gmail.com>
2021-02-04 19:08:13 -0300

    Update server.py

--- server.py
+++ server.py
@@ -13,19 +13,19 @@ def html_page(page_name):
     return render_template(page_name)
 
 def write_to_file(data):
-	with open('database.txt', mode='a') as database:
-		email = data['email']
-		subject = data['subject']
-		message = data['message']
-		file = database.write(f'\n{email},{subject},{message}')
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'\n{email},{subject},{message}')
 
 def write_to_csv(data):
-	with open('database.csv', mode='a') as database2:
-		email = data['email']
-		subject = data['subject']
-		message = data['message']
-		csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting = csv.QUOTE_MINIMAL)
-		csv_writer.writerow([email,subject,message])
+    with open('database.csv', mode='a') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting = csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
